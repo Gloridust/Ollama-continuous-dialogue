1421947a4d3bb6c8d7c75d019096b7833b159a49
Gloridust <Gloridust@gmail.com>
2024-03-03 23:12:23 +0800

    Create start.py

---
+++ start.py
@@ -0,0 +1,21 @@
+import ollama
+
+def get_response(message):
+    model_name = 'gemma:7b'
+    response = ollama.chat(
+        model=model_name, 
+        messages=message,
+        stream=False,
+    )
+    output_text = response['message']['content']
+    received_message = response['message']
+    return output_text, received_message
+
+message_history = []
+while True:
+    user_input_text = input("Input:")
+    message = {'role': 'user', 'content': user_input_text}
+    message_history.append(message)
+    output_text, received_message = get_response(message_history)
+    message_history.append(received_message)
+    print(output_text)
